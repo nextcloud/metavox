2344810687ddbfaebff24baf590c4a5c51796348
Sam428-png <s.d.ditmeijer@hva.nl>
2025-09-18 08:20:16 +0200

    Release 1.1.0
    ### Added
    - Native Nextcloud controls for improved integration and consistency with the Nextcloud design system
    
    ### Changed
    - Refactored UI components to use the Nextcloud design language
    - Improved stability and maintainability
    
    ### Fixed
    - Minor bugs and performance issues
    - Not able to edit text in File metadata text field

---
+++ src/components/fields/TextFieldInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <NcTextField
+    :value="modelValue"
+    @update:value="$emit('update:modelValue', $event)"
+    :required="required"
+    :placeholder="field.field_label" />
+</template>
+
+<script>
+import { NcTextField } from '@nextcloud/vue'
+
+export default {
+  name: 'TextFieldInput',
+  components: { NcTextField },
+  props: {
+    modelValue: String,
+    field: Object,
+    required: Boolean
+  },
+  emits: ['update:modelValue']
+}
+</script>
